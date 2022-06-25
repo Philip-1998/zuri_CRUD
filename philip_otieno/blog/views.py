00e33318e724be3a6b39f2d38b15c25be035b151
philip otieno <philipoti2012@gmail.com>
2022-06-26 01:30:03 +0300

    add views and urls

--- philip_otieno/blog/views.py
+++ philip_otieno/blog/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
 
+from django.shortcuts import render
+from django.views.generic import ListView,CreateView,DetailView,UpdateView,DeleteView
+from .models import Post
+from django.urls import reverse_lazy
 # Create your views here.
+
+
+class PostListView(ListView):
+    model = Post 
+
+
+class PostDetailView(DetailView):
+    model = Post
+
+
+class PostCreateView(CreateView):
+    model = Post
+    fields = "__all__"
+    success_url = reverse_lazy("blog:all")
+
+
+class PostUpdateView(UpdateView):
+    model = Post
+    fields = "__all__"
+    success_url = reverse_lazy("blog:all")
+
+
+class PostDeleteView(DeleteView):
+    model = Post
+    fields = "__all__"
+    success_url = reverse_lazy("blog:all")
+
+
+
+
